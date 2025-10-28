f03f31138ae3a93d7e7e225a13a5e080f837d092
LukeisStuff <lukeisawesome03@gmail.com>
2025-10-28 19:41:57 -0400

    finish reset for 7.3

---
+++ src/main/java/teamport/moonmod/MoonModels.java
@@ -0,0 +1,35 @@
+package teamport.moonmod;
+
+import net.minecraft.client.render.EntityRenderDispatcher;
+import net.minecraft.client.render.TileEntityRenderDispatcher;
+import net.minecraft.client.render.block.color.BlockColorDispatcher;
+import net.minecraft.client.render.block.model.BlockModelDispatcher;
+import net.minecraft.client.render.item.model.ItemModelDispatcher;
+import turniplabs.halplibe.util.ModelEntrypoint;
+
+public class MoonModels implements ModelEntrypoint {
+	@Override
+	public void initBlockModels(BlockModelDispatcher dispatcher) {
+
+	}
+
+	@Override
+	public void initItemModels(ItemModelDispatcher dispatcher) {
+
+	}
+
+	@Override
+	public void initEntityModels(EntityRenderDispatcher dispatcher) {
+
+	}
+
+	@Override
+	public void initTileEntityModels(TileEntityRenderDispatcher dispatcher) {
+
+	}
+
+	@Override
+	public void initBlockColors(BlockColorDispatcher dispatcher) {
+
+	}
+}
---
+++ src/main/java/teamport/moonmod/MoonRecipes.java
@@ -0,0 +1,15 @@
+package teamport.moonmod;
+
+import turniplabs.halplibe.util.RecipeEntrypoint;
+
+public class MoonRecipes implements RecipeEntrypoint {
+	@Override
+	public void onRecipesReady() {
+
+	}
+
+	@Override
+	public void initNamespaces() {
+
+	}
+}
