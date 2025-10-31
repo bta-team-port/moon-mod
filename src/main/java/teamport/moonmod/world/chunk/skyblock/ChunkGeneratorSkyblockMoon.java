207a4900beb1d2ca98ee7cceff1e0ca63a92a441
LukeisStuff <lukeisawesome03@gmail.com>
2025-10-31 19:55:58 -0400

    moon skyblock world

---
+++ src/main/java/teamport/moonmod/world/chunk/skyblock/ChunkGeneratorSkyblockMoon.java
@@ -0,0 +1,55 @@
+package teamport.moonmod.world.chunk.skyblock;
+
+import net.minecraft.core.world.World;
+import net.minecraft.core.world.chunk.Chunk;
+import net.minecraft.core.world.generate.chunk.ChunkGenerator;
+import net.minecraft.core.world.generate.chunk.ChunkGeneratorResult;
+import teamport.moonmod.blocks.MoonBlocks;
+
+public class ChunkGeneratorSkyblockMoon extends ChunkGenerator {
+	public ChunkGeneratorSkyblockMoon(World world) {
+		super(world, new ChunkDecoratorSkyblockMoon());
+	}
+
+	public ChunkGeneratorResult doBlockGeneration(Chunk chunk) {
+		ChunkGeneratorResult result = new ChunkGeneratorResult();
+
+		int x;
+		int z;
+		for (x = -3; x <= 1; ++x) {
+			for (z = -2; z <= 3; ++z) {
+				this.tryPlace(chunk, result, x, 64, z, MoonBlocks.REGOLITH.id());
+				this.tryPlace(chunk, result, x, 65, z, MoonBlocks.REGOLITH.id());
+				this.tryPlace(chunk, result, x, 66, z, MoonBlocks.REGOLITH.id());
+			}
+		}
+
+		for (z = -1; z <= 2; ++z) {
+			if (z != -1 && z != 2) {
+				this.tryPlace(chunk, result, 2, 66, z, MoonBlocks.CHEESE.id());
+				this.tryPlace(chunk, result, 2, 67, z, MoonBlocks.PORTAL_MOON.id());
+				this.tryPlace(chunk, result, 2, 68, z, MoonBlocks.PORTAL_MOON.id());
+				this.tryPlace(chunk, result, 2, 69, z, MoonBlocks.PORTAL_MOON.id());
+				this.tryPlace(chunk, result, 2, 70, z, MoonBlocks.CHEESE.id());
+			} else {
+				this.tryPlace(chunk, result, 2, 66, z, MoonBlocks.CHEESE.id());
+				this.tryPlace(chunk, result, 2, 67, z, MoonBlocks.CHEESE.id());
+				this.tryPlace(chunk, result, 2, 68, z, MoonBlocks.CHEESE.id());
+				this.tryPlace(chunk, result, 2, 69, z, MoonBlocks.CHEESE.id());
+				this.tryPlace(chunk, result, 2, 70, z, MoonBlocks.CHEESE.id());
+			}
+		}
+
+		return result;
+	}
+
+	public boolean contains(Chunk chunk, int xBlock, int zBlock) {
+		return chunk.xPosition == xBlock >> 4 && chunk.zPosition == zBlock >> 4;
+	}
+
+	public void tryPlace(Chunk chunk, ChunkGeneratorResult result, int xBlock, int yBlock, int zBlock, int blockId) {
+		if (this.contains(chunk, xBlock, zBlock)) {
+			result.setBlock(xBlock & 15, yBlock, zBlock & 15, blockId);
+		}
+	}
+}
--- src/main/java/teamport/moonmod/world/chunk/skyblock/WorldTypeMoonSkyblock.java
+++ src/main/java/teamport/moonmod/world/chunk/skyblock/WorldTypeMoonSkyblock.java
@@ -1,10 +1,16 @@
 package teamport.moonmod.world.chunk.skyblock;
 
+import net.minecraft.core.world.World;
+import net.minecraft.core.world.generate.chunk.ChunkGenerator;
+import net.minecraft.core.world.type.WorldType;
 import teamport.moonmod.world.WorldTypeMoon;
 
 public class WorldTypeMoonSkyblock extends WorldTypeMoon {
-
-	public WorldTypeMoonSkyblock(Properties properties) {
+	public WorldTypeMoonSkyblock(WorldType.Properties properties) {
 		super(properties);
+	}
+
+	public ChunkGenerator createChunkGenerator(World world) {
+		return new ChunkGeneratorSkyblockMoon(world);
 	}
 }
